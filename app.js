6e7002a4574811bb2085fd527551e575499542bd
Joseph Atkins-Turkish <spacerat3004@gmail.com>
2023-12-06 01:33:28 -0800

    new approach

--- app.js
+++ app.js
@@ -62,84 +62,59 @@ const semitonesMap = {
   "semitone-6": 6,
 };
 
-const touches = {};
+// const touches = {};
 
-let stringHz = null;
-let semitoneNumber = 0;
+// let stringHz = null;
+// let semitoneNumber = 0;
 
-function playStringTouch(touch) {
-  const element = document.elementFromPoint(touch.clientX, touch.clientY);
-  if (!element) return;
-  const elementHz = stringFrequenciesMap[element.id];
-  if (!elementHz) return;
-  stringHz = elementHz;
-  const tone = stringHz * Math.pow(2, semitoneNumber / 12);
-
-  getAudioWorld().playTone(tone);
-}
-
-function playSemitoneTouch(touch) {
-  const element = document.elementFromPoint(touch.clientX, touch.clientY);
-  if (!element) return;
-  const semitoneNumber = semitonesMap[element.id];
-  if (semitoneNumber === undefined || stringHz === null) return;
-  const tone = stringHz * Math.pow(2, semitoneNumber / 12);
-  getAudioWorld().playTone(tone);
-}
-
-function stringTouchStart(event) {
-  event.preventDefault();
-  for (const touch of event.touches) {
-    touches[touch.identifier] = "string";
-    playStringTouch(touch);
-  }
-}
-
-function semitoneTouchStart(event) {
-  event.preventDefault();
-  for (const touch of event.touches) {
-    touches[touch.identifier] = "semitone";
-    playSemitoneTouch(touch);
-  }
-}
-
-function touchMove(event) {
-  document.getElementById("debug").innerHTML = JSON.stringify(
-    Array.from(event.touches).map((x) => ({
-      identifier: x.identifier,
-      clientX: x.clientX,
-      clientY: x.clientY,
-    })),
-    null,
-    2
+function getFrequencyFromTouches(touches) {
+  const stringTouch = touches.find((x) => x.target.id.startsWith("string-"));
+  if (!stringTouch) return null;
+  const stringElement = document.elementFromPoint(
+    stringTouch.clientX,
+    stringTouch.clientY
   );
-  for (const touch of event.touches) {
-    if (touches[touch.identifier] === "string") {
-      playStringTouch(touch);
-    }
-    if (touches[touch.identifier] === "semitone") {
-      playSemitoneTouch(touch);
-    }
+  const elementHz = stringFrequenciesMap[stringElement?.id];
+  if (!elementHz) return null;
+
+  // Play the highest held semitone touch
+  const semitoneTouch = touches
+    .filter((x) => x.target.id.startsWith("semitone-"))
+    .sort((a, b) => b.clientY - a.clientY)[0];
+  let semitoneNumber = 0;
+
+  if (semitoneTouch) {
+    const semitoneTouchElement = document.elementFromPoint(
+      semitoneTouch.clientX,
+      semitoneTouch.clientY
+    );
+    semitoneNumber = semitonesMap[semitoneTouchElement?.id] ?? 0;
+  }
+
+  const tone = elementHz * Math.pow(2, semitoneNumber / 12);
+  return tone;
+}
+
+function handleTouch(event) {
+  //   event.preventDefault();
+  const tone = getFrequencyFromTouches(Array.from(event.touches));
+  if (tone) {
+    getAudioWorld().playTone(tone);
+  } else {
+    getAudioWorld().stopTone();
   }
 }
 
-function touchEnd(event) {
-  for (const touch of event.changedTouches) {
-    if (touches[touch.identifier] === "string") {
-      getAudioWorld().stopTone();
-      delete touches[touch.identifier];
-      stringHz = null;
-    }
-    if (touches[touch.identifier] === "semitone") {
-      // TODO: play the highest held semitone touch
-      semitoneNumber = 0;
-      delete touches[touch.identifier];
-      if (stringHz !== null) {
-        getAudioWorld().playTone(stringHz);
-      }
-    }
-  }
-}
+const strings = document.querySelector("#strings");
+const semitones = document.querySelector("#semitones");
+
+strings.addEventListener("touchstart", handleTouch);
+semitones.addEventListener("touchstart", handleTouch);
+document.addEventListener("touchmove", handleTouch);
+document.addEventListener("touchend", handleTouch);
+document.addEventListener("touchcancel", handleTouch);
+// document.addEventListener("mousedown", handleTouch);
+// document.addEventListener("mouseup", handleTouch);
 
 document.querySelector("#start").addEventListener("click", () => {
   getAudioWorld().audioCtx.resume();
@@ -148,11 +123,97 @@ document.querySelector("#start").addEventListener("click", () => {
   document.querySelector("#semitones").classList.remove("hidden");
 });
 
-const strings = document.querySelector("#strings");
-const semitons = document.querySelector("#semitons");
+// strings.addEventListener("touchstart", stringTouchStart);
+// semitones.addEventListener("touchstart", semitoneTouchStart);
+// document.addEventListener("touchmove", touchMove);
+// document.addEventListener("touchend", touchEnd);
+// document.addEventListener("touchcancel", touchEnd);
 
-strings.addEventListener("touchstart", stringTouchStart);
-semitones.addEventListener("touchstart", semitoneTouchStart);
-document.addEventListener("touchmove", touchMove);
-document.addEventListener("touchend", touchEnd);
-document.addEventListener("touchcancel", touchEnd);
+// function playStringTouch(touch) {
+//   const element = document.elementFromPoint(touch.clientX, touch.clientY);
+//   if (!element) return;
+//   const elementHz = stringFrequenciesMap[element.id];
+//   if (!elementHz) return;
+//   stringHz = elementHz;
+//   const tone = stringHz * Math.pow(2, semitoneNumber / 12);
+
+//   getAudioWorld().playTone(tone);
+// }
+
+// function playSemitoneTouch(touch) {
+//   const element = document.elementFromPoint(touch.clientX, touch.clientY);
+//   if (!element) return;
+//   const semitoneNumber = semitonesMap[element.id];
+//   if (semitoneNumber === undefined || stringHz === null) return;
+//   const tone = stringHz * Math.pow(2, semitoneNumber / 12);
+//   getAudioWorld().playTone(tone);
+// }
+
+// function stringTouchStart(event) {
+//   event.preventDefault();
+//   for (const touch of event.touches) {
+//     touches[touch.identifier] = "string";
+//     playStringTouch(touch);
+//   }
+// }
+
+// function semitoneTouchStart(event) {
+//   event.preventDefault();
+//   for (const touch of event.touches) {
+//     touches[touch.identifier] = "semitone";
+//     playSemitoneTouch(touch);
+//   }
+// }
+
+// function touchMove(event) {
+//   document.getElementById("debug").innerHTML = JSON.stringify(
+//     Array.from(event.touches).map((x) => ({
+//       identifier: x.identifier,
+//       clientX: x.clientX,
+//       clientY: x.clientY,
+//     })),
+//     null,
+//     2
+//   );
+//   for (const touch of event.touches) {
+//     if (touches[touch.identifier] === "string") {
+//       playStringTouch(touch);
+//     }
+//     if (touches[touch.identifier] === "semitone") {
+//       playSemitoneTouch(touch);
+//     }
+//   }
+// }
+
+// function touchEnd(event) {
+//   document.getElementById("debug").innerHTML = JSON.stringify(
+//     Array.from(event.touches).map((x) => ({
+//       identifier: x.identifier,
+//       clientX: x.clientX,
+//       clientY: x.clientY,
+//     })),
+//     null,
+//     2
+//   );
+//   for (const touch of event.changedTouches) {
+//     if (touches[touch.identifier] === "string") {
+//       getAudioWorld().stopTone();
+//       delete touches[touch.identifier];
+//       stringHz = null;
+//     }
+//     if (touches[touch.identifier] === "semitone") {
+//       // TODO: play the highest held semitone touch
+//       semitoneNumber = 0;
+//       delete touches[touch.identifier];
+//       if (stringHz !== null) {
+//         getAudioWorld().playTone(stringHz);
+//       }
+//     }
+//   }
+// }
+
+// strings.addEventListener("touchstart", stringTouchStart);
+// semitones.addEventListener("touchstart", semitoneTouchStart);
+// document.addEventListener("touchmove", touchMove);
+// document.addEventListener("touchend", touchEnd);
+// document.addEventListener("touchcancel", touchEnd);
